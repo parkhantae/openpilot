62fb7b9744ddc8c007aa207d36d0c57605a9fc29
ajouatom <ajouatom@gmail.com>
2024-11-02 18:34:57 +0900

    fix...

--- opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
@@ -308,7 +308,6 @@ def create_adrv_messages(CP, packer, CAN, frame, CC, CS, hud_control):
 
       if CS.adrv_info_200 is not None:
         values = CS.adrv_info_200
-        print("adrv_info_200 = ", CS.adrv_info_200)
         values["TauGapSet"] = hud_control.leadDistanceBars
         ret.append(packer.make_can_msg("ADRV_0x200", CAN.ECAN, values))
     return ret
