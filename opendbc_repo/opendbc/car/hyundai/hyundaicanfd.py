5a6af8499777537a79a7808ee934c49a7150f80e
ajouatom <ajouatom@gmail.com>
2024-11-02 18:25:32 +0900

    fix..

--- opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
@@ -221,7 +221,7 @@ def create_acc_control(packer, CAN, enabled, accel_last, accel, stopping, gas_ov
     "SET_ME_2": 0x4,
     #"SET_ME_3": 0x3,
     "SET_ME_TMP_64": 0x64,
-    "DISTANCE_SETTING": hud_control.leadDistanceBars# + 5,
+    "DISTANCE_SETTING": hud_control.leadDistanceBars, # + 5,
     "CRUISE_STANDSTILL": 1 if stopping and CS.out.cruiseState.standstill else 0,
   }
 
