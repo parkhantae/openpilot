56e9d62d6becc2761fba361160a97dd1941def09
ajouatom <ajouatom@gmail.com>
2024-11-03 18:29:00 +0900

    ff

--- opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
@@ -348,6 +348,8 @@ def create_adrv_messages(CP, packer, CAN, frame, CC, CS, hud_control):
           values["CRUISE_INFO5_SET1"] = 1 if cruise_enabled else 0
           values["SET4_HWAY_ELSE_3"] = 3
 
+          values["START_READY_INFO_MAYBE"] = 0
+
           values["NEW_SIGNAL_7"] = 0
           values["NEW_SIGNAL_5"] = 0
           values["LANE_ASSIST_CONCERNED"] = 0
