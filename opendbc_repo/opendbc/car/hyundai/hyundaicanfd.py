b07f54f66673b8c2d9313f4bba0d6e78a6becae3
ajouatom <ajouatom@gmail.com>
2024-11-03 17:22:58 +0900

    ff

--- opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
@@ -349,8 +349,8 @@ def create_adrv_messages(CP, packer, CAN, frame, CC, CS, hud_control):
 
       if CS.adrv_info_1ea is not None:
         values = CS.adrv_info_1ea
-        values["HDA_MODE1"] = 28 #8
-        values["HDA_MODE2"] = 5 #0
+        values["HDA_MODE1"] = 8
+        values["HDA_MODE2"] = 0
         ret.append(packer.make_can_msg("ADRV_0x1ea", CAN.ECAN, values))
     return ret
   else:
